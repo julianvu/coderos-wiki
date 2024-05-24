35ff30643ca230c5168613232f52ad56c88a383a
Julian Vu <julianvu@outlook.com>
2024-05-24 15:43:08 -0700

    Deploy wiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wiki.coderos.dev',
   integrations: [starlight({
     title: 'Coderos Wiki',
     customCss: [
         './src/tailwind.css',
     ],
     social: {
-      github: 'https://github.com/withastro/starlight'
+      github: 'https://github.com/julianvu/coderos-wiki'
     },
     sidebar: [{
       label: 'The Wiki',
